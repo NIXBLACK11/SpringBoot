6aa2f543592b5926b4de0346cd5ce36daaceb9a7
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-03 18:46:28 +0530

    h

--- content-calender/src/main/java/dev/siddharth/contentcalender/repository/ContentCollectionRepository.java
+++ content-calender/src/main/java/dev/siddharth/contentcalender/repository/ContentCollectionRepository.java
@@ -1,4 +1,19 @@
 package dev.siddharth.contentcalender.repository;
 
+
+import dev.siddharth.contentcalender.model.Content;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class ContentCollectionRepository {
+    private final List<Content> = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+
+    }
+
+    public List<Content> find
 }
