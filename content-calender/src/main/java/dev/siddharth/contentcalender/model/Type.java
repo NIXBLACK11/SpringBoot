b40385b239989417249040f1ec1942ea5477622e
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-03 18:30:32 +0530

    Type model

--- content-calender/src/main/java/dev/siddharth/contentcalender/model/Type.java
+++ content-calender/src/main/java/dev/siddharth/contentcalender/model/Type.java
@@ -1,5 +1,8 @@
 package dev.siddharth.contentcalender.model;
 
 public enum Type {
-
+    ARTICLE,
+    VIDEO,
+    COURSE,
+    CONFERENCE_TALK
 }
---
+++ content-calender/src/main/java/dev/siddharth/contentcalender/repository/ContentCollectionRepository.java
@@ -0,0 +1,4 @@
+package dev.siddharth.contentcalender.repository;
+
+public class ContentCollectionRepository {
+}
