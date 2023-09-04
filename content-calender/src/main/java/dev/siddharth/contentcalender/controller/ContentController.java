6afe1a080bc4ee5ba8612a8b09f2a73901666bb4
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-04 18:12:36 +0530

    get post

--- content-calender/src/main/java/dev/siddharth/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/dev/siddharth/contentcalender/controller/ContentController.java
@@ -1,10 +1,36 @@
 package dev.siddharth.contentcalender.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.siddharth.contentcalender.model.Content;
+import dev.siddharth.contentcalender.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
 
+    @GetMapping("/{id}")
+    public Content findId(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+    // Create Read Update Delete -
 }
--- content-calender/src/main/java/dev/siddharth/contentcalender/repository/ContentCollectionRepository.java
+++ content-calender/src/main/java/dev/siddharth/contentcalender/repository/ContentCollectionRepository.java
@@ -2,18 +2,48 @@ package dev.siddharth.contentcalender.repository;
 
 
 import dev.siddharth.contentcalender.model.Content;
+import dev.siddharth.contentcalender.model.Status;
+import dev.siddharth.contentcalender.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
 
     }
 
-    public List<Content> find
+    public List<Content> findAll() {
+        return contentList;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.add(content);
+    }
+    @PostConstruct
+    private void init() {
+        Content content = new Content(
+                1,
+                "My First Blog Post",
+                "blog 1",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        contentList.add(content);
+    }
+
 }
